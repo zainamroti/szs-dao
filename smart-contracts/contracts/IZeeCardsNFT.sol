07663415abfd086ed0c0e4cc6ad433485894188f
zainamroti <sayedzainjeelani@gmail.com>
2022-04-30 05:46:32 +0500

    Add both front & contracts to git.

---
+++ smart-contracts/contracts/IZeeCardsNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * Minimal interface for ZeeCardsNFT containing only two functions
+ * that we are interested in
+ */
+interface IZeeCardsNFT {
+    /// @dev balanceOf returns the number of NFTs owned by the given address
+    /// @param owner - address to fetch number of NFTs for
+    /// @return Returns the number of NFTs owned
+    function balanceOf(address owner) external view returns (uint256);
+
+    /// @dev tokenOfOwnerByIndex returns a tokenID at given index for owner
+    /// @param owner - address to fetch the NFT TokenID for
+    /// @param index - index of NFT in owned tokens array to fetch
+    /// @return Returns the TokenID of the NFT
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
---
+++ smart-contracts/scripts/deploy.js
@@ -0,0 +1,35 @@
+const { ethers } = require("hardhat");
+const { ZEE_CARDS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the CryptoDevsDAO contract`
+  const ZeeCardsDAO = await ethers.getContractFactory("ZeeCardsDAO");
+  const zeeCardsDAO = await ZeeCardsDAO.deploy(
+    fakeNftMarketplace.address,
+    ZEE_CARDS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your account has at least 1 ETH in it's account
+      // Change this value as you want
+      value: ethers.utils.parseEther("0.1"),
+    }
+  );
+  await zeeCardsDAO.deployed();
+
+  console.log("ZeeCardsDAO deployed to: ", zeeCardsDAO.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
